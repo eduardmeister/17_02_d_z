685dbe0668ce972d47a86b59944c938985f442fb
eddy106 <119332233+eduardmeister@users.noreply.github.com>
2023-02-11 00:38:42 +0100

    v4

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-# app.py
-
 from flask import Flask, request
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -48,7 +47,8 @@ class MovieSchema(Schema):
     director_id = fields.Int()
 
 
-movie_schema = MovieSchema
+movie_schema = MovieSchema()
+
 
 api = Api(app)
 movie_ns = api.namespace('movies')
@@ -57,8 +57,22 @@ movie_ns = api.namespace('movies')
 @movie_ns.route('/')
 class MoviesView(Resource):
     def get(self):
-        movies = Movie.query.all()
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        stmt = Movie.query
+        if director_id:
+            stmt = stmt.filter(Movie.director_id == director_id)
+        if genre_id:
+            stmt = stmt.filter(Movie.genre_id == genre_id)
+        movies = stmt.all()
         return movie_schema.dump(movies, many=True), 200
+
+    def post(self):
+        movie_data = request.json
+        new_movie = (Movie(**movie_data))
+        db.session.add(new_movie)
+        db.session.commit()
+        return '', 201
 
 
 @movie_ns.route('/<int:mid>')
@@ -68,6 +82,36 @@ class MovieView(Resource):
         if not movie:
             return '', 404
         return movie_schema.dump(movie), 200
+
+
+    def put(self, mid: int):
+        movie = Movie.query.get(mid)
+
+        if not movie:
+            return '', 404
+
+        movie_data = request.json
+        movie.title = movie_data.get('title')
+        movie.description = movie_data.get('description')
+        movie.trailer = movie_data.get('trailer')
+        movie.year = movie_data.get('year')
+        movie.rating = movie_data.get('rating')
+        movie.genre_id = movie_data.get('genre_id')
+        movie.director_id = movie_data.get('director_id')
+
+        db.session.add(movie)
+        db.session.commit()
+        return '', 204
+
+
+    def delete(self, mid: int):
+        movie = Movie.query.get(mid)
+        if not movie:
+            return '', 204
+
+        db.session.delete(movie)
+        db.session.commit()
+        return '', 204
 
 
 if __name__ == '__main__':
